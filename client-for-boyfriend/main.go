250f84ddb776d9ae614fd70b4691db60edd40e4c
Piorosen <chacha@udon.party>
2024-06-17 05:02:43 +0900

    yaho!

--- client-for-boyfriend/main.go
+++ client-for-boyfriend/main.go
@@ -27,14 +27,15 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
+	"os"
+	"strconv"
 
 	_ "image/jpeg" // Register JPEG format
 
 	"github.com/Piorosen/boyfriend/client-for-boyfriend/service"
-	"github.com/disintegration/imaging"
+	"github.com/subosito/gotenv"
 )
 
 type Flags struct {
@@ -49,60 +50,46 @@ const (
 	outputSize = 1000
 )
 
-func parseFlags() Flags {
-	var flags Flags
-	// https://github.com/NVIDIA/triton-inference-server/tree/master/docs/examples/model_repository/simple
-	flag.StringVar(&flags.ModelName, "m", "densenet_onnx", "Name of model being served. (Required)")
-	flag.StringVar(&flags.ModelVersion, "x", "1", "Version of model. Default: Latest Version.")
-	flag.IntVar(&flags.BatchSize, "b", 1, "Batch size. Default: 1.")
-	flag.StringVar(&flags.URL, "u", "localhost:8001", "Inference Server URL. Default: localhost:8001")
-	flag.Parse()
-	return flags
-}
+func get_environment() (Flags, error) {
+	name := os.Getenv("MODEL_NAME_FROM_TRITON")
+	version := os.Getenv("MODEL_VERSION_FROM_TRITON")
+	batch := os.Getenv("MODEL_BATCH_SIZE_FROM_TRITON")
+	host := os.Getenv("INFERENCE_SERVER_TO_TRITON")
 
-// Function to read an image file, resize it to 224x224 pixels, and output a float array
-func ImageToFloatArray(fileName string) ([]float32, error) {
-	// Open a test image.
-	src, err := imaging.Open(fileName)
+	if name == "" {
+		return Flags{}, fmt.Errorf("MODEL_NAME_FROM_TRITON is not set")
+	}
+	if version == "" {
+		return Flags{}, fmt.Errorf("MODEL_VERSION_FROM_TRITON is not set")
+	}
+	if batch == "" {
+		return Flags{}, fmt.Errorf("MODEL_BATCH_SIZE_FROM_TRITON is not set")
+	}
+	if host == "" {
+		return Flags{}, fmt.Errorf("INFERENCE_SERVER_TO_TRITON is not set")
+	}
+	num_batch, err := strconv.ParseInt(batch, 10, 32)
 	if err != nil {
-		log.Fatalf("failed to open image: %v", err)
+		return Flags{}, fmt.Errorf("BATCHSIZE ERROR %v", err)
 	}
-	// Resize the cropped image to width = 200px preserving the aspect ratio.
-	src = imaging.Resize(src, 224, 224, imaging.Lanczos)
-	width := 224
-	height := 224
-	channels := 3
-	pixels := make([]float32, channels*width*height)
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			r, g, b, _ := src.At(x, y).RGBA()
-			// NCHW
-			// Normalize to [-1, 1] and store in the array
-			pixels[0*width*height+y*width+x] = (float32(r&0xff) / 127.5) - 1
-			pixels[1*width*height+y*width+x] = (float32(g&0xff) / 127.5) - 1
-			pixels[2*width*height+y*width+x] = (float32(b&0xff) / 127.5) - 1
-			// NHWC
-			// pixels[224*3*y+3*x+0] = (float32(r&0xff) / 127.5) - 1
-			// pixels[224*3*y+3*x+1] = (float32(g&0xff) / 127.5) - 1
-			// pixels[224*3*y+3*x+2] = (float32(b&0xff) / 127.5) - 1
-		}
-	}
-	// fmt.Printf("%v", imaging.Save(src, "asdf.jpg"))
-	return pixels, nil
+	return Flags{
+		ModelName:    name,
+		ModelVersion: version,
+		BatchSize:    int(num_batch),
+		URL:          host,
+	}, nil
+
 }
 
 func main() {
-	fileName := "resources/mug.jpg"
-	floatArray, err := ImageToFloatArray(fileName)
+	gotenv.Load()
+	env, err := get_environment()
 	if err != nil {
-		log.Fatalf("Error processing image: %v", err)
+		log.Fatalf("Error load environemnt: %v", err)
 	}
 
-	FLAGS := parseFlags()
-	fmt.Println("FLAGS:", FLAGS)
-
 	client := service.NewClient()
-	client.Open(FLAGS.URL)
+	client.Open(env.URL)
 	defer client.Close()
 
 	serverLiveResponse := client.ServerLiveRequest()
@@ -111,14 +98,20 @@ func main() {
 	serverReadyResponse := client.ServerReadyRequest()
 	fmt.Printf("Triton Health - Ready: %v\n", serverReadyResponse.Ready)
 
-	modelMetadataResponse := client.ModelMetadataRequest(FLAGS.ModelName, FLAGS.ModelVersion)
+	modelMetadataResponse := client.ModelMetadataRequest(env.ModelName, env.ModelVersion)
 	fmt.Println(modelMetadataResponse)
+
+	fileName := "resources/mug.jpg"
+	floatArray, err := ImageToFloatArray(fileName)
+	if err != nil {
+		log.Fatalf("Error processing image: %v", err)
+	}
 	input := Preprocess(floatArray)
 	// /* We use a simple model that takes 2 input tensors of 16 integers
 	// each and returns 2 output tensors of 16 integers each. One
 	// output tensor is the element-wise sum of the inputs and one
 	// output is the element-wise difference. */
-	inferResponse := client.ModelInferRequest(input, FLAGS.ModelName, FLAGS.ModelVersion)
+	inferResponse := client.ModelInferRequest(input, env.ModelName, env.ModelVersion)
 	// /* We expect there to be 2 results (each with batch-size 1). Walk
 	// over all 16 result elements and print the sum and difference
 	// calculated by the model. */
--- client-for-boyfriend/utils.go
+++ client-for-boyfriend/utils.go
@@ -2,12 +2,48 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"encoding/binary"
+	"log"
 	"math"
 	"os"
 	"strings"
+
+	"github.com/disintegration/imaging"
 )
+
+// TODO: https://github.com/sunhailin-Leo/triton-service-go
+// TODO: https://github.com/triton-inference-server/server/issues/6463
+// TOOD: https://github.com/triton-inference-server/client/blob/main/src/c%2B%2B/examples/image_client.cc
+// Function to read an image file, resize it to 224x224 pixels, and output a float array
+func ImageToFloatArray(fileName string) ([]float32, error) {
+	// Open a test image.
+	src, err := imaging.Open(fileName)
+	if err != nil {
+		log.Fatalf("failed to open image: %v", err)
+	}
+	// Resize the cropped image to width = 200px preserving the aspect ratio.
+	src = imaging.Resize(src, 224, 224, imaging.Lanczos)
+	width := 224
+	height := 224
+	channels := 3
+	pixels := make([]float32, channels*width*height)
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			r, g, b, _ := src.At(x, y).RGBA()
+			// NCHW
+			// Normalize to [-1, 1] and store in the array
+			pixels[0*width*height+y*width+x] = (float32(r&0xff) / 127.5) - 1
+			pixels[1*width*height+y*width+x] = (float32(g&0xff) / 127.5) - 1
+			pixels[2*width*height+y*width+x] = (float32(b&0xff) / 127.5) - 1
+			// NHWC
+			// pixels[224*3*y+3*x+0] = (float32(r&0xff) / 127.5) - 1
+			// pixels[224*3*y+3*x+1] = (float32(g&0xff) / 127.5) - 1
+			// pixels[224*3*y+3*x+2] = (float32(b&0xff) / 127.5) - 1
+		}
+	}
+	// fmt.Printf("%v", imaging.Save(src, "asdf.jpg"))
+	return pixels, nil
+}
 
 // Convert int32 input data into raw bytes (assumes Little Endian)
 func Preprocess(inputs []float32) []byte {
@@ -26,18 +62,10 @@ func Preprocess(inputs []float32) []byte {
 func Postprocess(inferResponse []byte) []float32 {
 	outputData0 := make([]float32, outputSize)
 	for i := 0; i < outputSize; i++ {
-		// f := binary.LittleEndian.Uint32(inferResponse[i*4 : i*4+4])
-		// outputData0[i] = math.Float32frombits(f)
-		f := readFloat32(inferResponse[i*4 : i*4+4])
-		outputData0[i] = f
+		f := binary.LittleEndian.Uint32(inferResponse[i*4 : i*4+4])
+		outputData0[i] = math.Float32frombits(f)
 	}
 	return outputData0
-}
-func readFloat32(fourBytes []byte) float32 {
-	buf := bytes.NewBuffer(fourBytes)
-	var retval float32
-	binary.Read(buf, binary.LittleEndian, &retval)
-	return retval
 }
 
 // LoadLabels loads labels from a file
